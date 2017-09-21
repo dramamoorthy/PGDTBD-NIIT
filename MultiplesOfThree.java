4319d2daff0df9c6a805f54034774b427fd4238b
Dinesh Ramamoorthy <dineshr4981@gmail.com>
2017-09-21 12:46:11 +0530

    Add files via upload

---
+++ MultiplesOfThree.java
@@ -0,0 +1,15 @@
+//To print numbers from 1 to 10 without multiples of 3
+class MultiplesOfThree
+{
+public static void main(String[] args)
+{
+int i;
+for(i=0;i<11;i++)
+{
+if(i%3!=0)
+{
+System.out.println(i);
+}
+}
+}
+}
